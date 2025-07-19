c822202cd4afc7bbc33630219c07c39ef349fe42
Mark Heramis <chumheramis@gmail.com>
2025-07-19 23:28:36 +0800

    add logger for debugging
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod process;
 mod tui;
-
+use log4rs;
 // Entry point for the application
 fn main() {
+
+    log4rs::init_file("log4rs.yml", Default::default()).unwrap();
+
     if let Err(e) = tui::run_tui() {
         eprintln!("Error: {}", e);
     }
